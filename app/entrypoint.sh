0db54aefb94c438234cf832bb577e1ce48e497df
Ian Levesque <ian@quantopian.com>
2017-12-21 12:26:19 -0500

    our default MO is to use SLACK_TOKEN in env now, so no need to be verbose

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-if [[ ! -s /app/.env ]]; then
-    echo "NOTE: using SLACK_TOKEN ($SLACK_TOKEN) provided in ENV"
-    echo "SLACK_TOKEN=${SLACK_TOKEN}" > /app/.env
-fi
-
 ./wait-for-it.sh -s -t 900 localhost:5432 -- npm start
